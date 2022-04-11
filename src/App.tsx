f737a11048f49a29edf72fcdc1756c0345f9564e
Chloe <chloee@ChloeuicBookAir.cg.shawcable.net>
2022-04-10 18:22:32 -0600

    after Responsive modal & Close Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import RWDModal from './ModalPopUp/RWDModal';
 
 function App() {
+
+  const [isModalVisibla, setIsModalVisible] = useState<boolean>(false);
+
+  const toggleModal = () => {
+    setIsModalVisible(wasModalVisible => !wasModalVisible)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <button onClick={toggleModal}>Show Modal</button>
+     <RWDModal header="Login" message='Please Login!' isModalVisible={isModalVisibla} onBackdropClick={toggleModal} />
     </div>
   );
 }
---
+++ src/ModalPopUp/BaseModalWrapper.tsx
@@ -0,0 +1,38 @@
+import React, { MouseEventHandler } from 'react'
+import Modal from './Modal';
+import { Header, Message, CloseSign } from './ModalPopUpStyles';
+
+export interface BaseModalWrapperProps {
+  isModalVisible: boolean;
+  onBackdropClick: () => void;
+  header: string;
+  message?: string;
+}
+
+interface ComponentsProps {
+  ContainerComponent: React.ComponentType<{}>,
+  CloseButtonComponent: React.ComponentType<{
+    onClick?: MouseEventHandler<any>
+  }>
+}
+
+type Props = BaseModalWrapperProps & ComponentsProps;
+
+const BaseModalWrapper: React.FC<Props> = ({onBackdropClick, isModalVisible, header, message, ContainerComponent, CloseButtonComponent}) => {
+    if(!isModalVisible) {
+      return null;
+    }
+     return (
+     <Modal onBackdropClick={onBackdropClick}>
+       <ContainerComponent>
+         <CloseButtonComponent onClick={onBackdropClick}>
+           <CloseSign />
+         </CloseButtonComponent>
+         <Header>{header}</Header>
+         {message && <Message>{message}</Message>}
+       </ContainerComponent>
+     </Modal>
+  )
+}
+
+export default BaseModalWrapper
